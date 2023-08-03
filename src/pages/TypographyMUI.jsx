3abb3edadbc737c9f1f8e6e94aa9d21577e9536e
Anas Shofyan Martunis Fateh <shofyananasmf@gmail.com>
2023-08-03 14:13:10 +0700

    feat: add custom theme typography

--- src/pages/TypographyMUI.jsx
+++ src/pages/TypographyMUI.jsx
@@ -1,9 +1,55 @@
+import { ThemeProvider } from "@mui/material/styles";
 import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import Typography from "@mui/material/Typography";
+import theme from "../../src/themes/theme";
+
 function TypographyMUI() {
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    bgcolor: "background.paper",
+    boxShadow: 24,
+    p: 4,
+  };
   return (
-    <div>
-      <Button variant="contained">Hello World</Button>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <div style={style}>
+          <div>
+            <Typography variant="h1">h1. Heading</Typography>
+            <hr />
+            <Typography variant="h2">h2. Heading</Typography>
+            <Typography variant="h3">h3. Heading</Typography>
+            <Typography variant="h4">h4. Heading</Typography>
+            <Typography variant="h5">h5. Heading</Typography>
+            <Typography variant="h6">h6. Heading</Typography>
+            <Typography variant="body1">body1. Body</Typography>
+            <br />
+            <Typography variant="body2">body2. Body</Typography>
+            <br />
+            <Typography variant="body3">body3. Body</Typography>
+            <br />
+            <Typography variant="body4">body4. Body</Typography>
+            <br />
+            <Typography variant="p1">p1. Paragraph</Typography>
+            <br />
+            <Typography variant="p2">p2. Paragraph</Typography>
+            <br />
+            <Typography variant="p3">p3. Paragraph</Typography>
+            <br />
+            <Typography variant="overline">Overline</Typography>
+          </div>
+          <Tooltip title="Delete">
+            <Button variant="contained" style={{ borderRadius: "100px" }}>
+              Hello World
+            </Button>
+          </Tooltip>
+        </div>
+      </ThemeProvider>
+    </>
   );
 }
 
---
+++ src/themes/theme.js
@@ -0,0 +1,97 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  typography: (defaultTypography = 400) => ({
+    h1: {
+      fontSize: "53px",
+      fontStyle: "normal",
+      fontWeight: defaultTypography.fontWeight,
+      lineHeight: "140%",
+    },
+    h2: {
+      fontSize: "43px",
+      fontStyle: "normal",
+      fontWeight: defaultTypography.fontWeight,
+      lineHeight: "140%",
+    },
+    h3: {
+      fontSize: "34px",
+      fontStyle: "normal",
+      fontWeight: defaultTypography.fontWeight,
+      lineHeight: "140%",
+    },
+    h4: {
+      fontSize: "27px",
+      fontStyle: "normal",
+      fontWeight: defaultTypography.fontWeight,
+      lineHeight: "140%",
+    },
+    h5: {
+      fontSize: "22px",
+      fontStyle: "normal",
+      fontWeight: defaultTypography.fontWeight,
+      lineHeight: "140%",
+    },
+    h6: {
+      fontSize: "18px",
+      fontStyle: "normal",
+      fontWeight: defaultTypography.fontWeight,
+      lineHeight: "140%",
+    },
+    body1: {
+      fontSize: "16px",
+      fontStyle: "normal",
+      fontWeight: defaultTypography.fontWeight,
+      lineHeight: "140%",
+    },
+    body2: {
+      fontSize: "14px",
+      fontStyle: "normal",
+      fontWeight: defaultTypography.fontWeight,
+      lineHeight: "140%",
+    },
+    body3: {
+      fontSize: "13px",
+      fontStyle: "normal",
+      fontWeight: defaultTypography.fontWeight,
+      lineHeight: "140%",
+    },
+    body4: {
+      fontSize: "10px",
+      fontStyle: "normal",
+      fontWeight: defaultTypography.fontWeight,
+      lineHeight: "140%",
+    },
+    p1: {
+      fontSize: "16px",
+      fontStyle: "normal",
+      fontWeight: defaultTypography.fontWeight,
+      lineHeight: "140%",
+      letterSpacing: "0.32px",
+    },
+    p2: {
+      fontSize: "14px",
+      fontStyle: "normal",
+      fontWeight: defaultTypography.fontWeight,
+      lineHeight: "140%",
+      letterSpacing: "0.28px",
+    },
+    p3: {
+      fontSize: "14px",
+      fontStyle: "normal",
+      fontWeight: defaultTypography.fontWeight,
+      lineHeight: "140%",
+      letterSpacing: "0.28px",
+    },
+    overline: {
+      fontSize: "10px",
+      fontStyle: "normal",
+      fontWeight: defaultTypography.fontWeight,
+      lineHeight: "140%",
+      letterSpacing: "0.28px",
+      textTransform: "uppercase",
+    },
+  }),
+});
+
+export default theme;
